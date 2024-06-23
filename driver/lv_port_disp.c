3839169ba8e20da6585d079a0e7c54f7ef38122d
hao@thinkbook <33110820@qq.com>
2024-06-23 23:52:31 +0800

    更新：增加indev.c/h注册进driver，注释原先模板重命名disp.c/h进driver，增加逸雨代码，我tm没管控工程文件T^T

--- driver/lv_port_disp.c
+++ driver/lv_port_disp.c
@@ -4,12 +4,12 @@
  */
 
 /*Copy this file as "lv_port_disp.c" and set this value to "1" to enable content*/
-#if 0
+#if 1
 
 /*********************
  *      INCLUDES
  *********************/
-#include "lv_port_disp_template.h"
+#include "lv_port_disp.h"
 #include <stdbool.h>
 #include "st7789.h"
 #include "lv_mcu_driver.h"
